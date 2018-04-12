558f5c6ea84262350b40662257059d64776381b6
Mercy-Muchai <mercy.muchai@andela.com>
2018-04-12 12:22:35 +0300

    chore(model updates) - update models to take todo & items - update database_url
    Signed-off-by: Mercy-Muchai <mercy.muchai@andela.com>

--- models.py
+++ models.py
@@ -1,46 +1,40 @@
 from flask import Flask
-from marshmallow import Schema, fields, pre_load, validate
-from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-
-ma = Marshmallow()
 db = SQLAlchemy()
 
 
-class Comment(db.Model):
-    __tablename__ = 'comments'
+class Item(db.Model):
+    __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.String(250), nullable=False)
+    item = db.Column(db.String(250), nullable=False)
     creation_date = db.Column(
         db.TIMESTAMP, server_default=db.func.current_timestamp(),
         nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey(
-        'categories.id', ondelete='CASCADE'), nullable=False)
-    category = db.relationship('Category', backref=db.backref('comments',
-                                                              lazy='dynamic'))
+    todo_id = db.Column(db.Integer, db.ForeignKey(
+        'todos.id', ondelete='CASCADE'), nullable=False)
+    todo = db.relationship('Todo', backref=db.backref('items',
+                                                      lazy='dynamic'))
 
-    def __init__(self, comment, category_id):
-        self.comment = comment
-        self.category_id = category_id
+    def __init__(self, item, todo_id):
+        self.item = item
+        self.todo_id = todo_id
 
 
-class Category(db.Model):
-    __tablename__ = 'categories'
+class Todo(db.Model):
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True, nullable=False)
 
     def __init__(self, name):
+        """ Initialize with name of todo """
         self.name = name
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-class CategorySchema(ma.Schema):
-    id = fields.Integer()
-    name = fields.String(required=True)
-
-
-class CommentSchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
-    category_id = fields.Integer(required=True)
-    comment = fields.String(required=True, validate=validate.Length(1))
-    creation_date = fields.DateTime()
+    @staticmethod
+    def get_all(todo_id):
+        """Return all items belonging to a todo."""
+        return Item.query.filter_by(belongs_to=todo_id)
